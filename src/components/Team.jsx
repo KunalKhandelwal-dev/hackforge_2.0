9f5d9ec07fceac2a37beccde06ccd97e0c4f67fc
kunal131005 <kunalkhandelwal155@gmail.com>
2026-04-04 18:40:41 +0530

    Comment out Riya's entry in the core team section

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -48,12 +48,12 @@ const coreTeam = [
     image: './images/team/muskan.webp',
     linkedin: 'https://www.linkedin.com/in/kaushikmuskan29',
   },
-  {
-    name: 'Riya',
-    role: 'BTech 2nd Year',
-    image: './images/team/riya.webp',
-    linkedin: 'https://www.linkedin.com/in/riya-rana-b752a6327/',
-  },
+  // {
+  //   name: 'Riya',
+  //   role: 'BTech 2nd Year',
+  //   image: './images/team/riya.webp',
+  //   linkedin: 'https://www.linkedin.com/in/riya-rana-b752a6327/',
+  // },
   {
     name: 'Nera',
     role: 'BTech 2nd Year',
